bbac7c4a2711576368df078534cb520e5c47e76f
Jose Luis <joseluiszero@gmail.com>
2016-07-05 15:19:51 +0200

    Fixed upgrader

--- dist/role.upgrader.js
+++ dist/role.upgrader.js
@@ -7,24 +7,11 @@
         creep.moveTo(Game.spawns.Spawn1);
         Game.spawns.Spawn1.transferEnergy(creep);
       }else{
-        var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-          filter: function(object){
-                  //if(object.structureType != STRUCTURE_ROAD ) {
-                  //   return false;
-                  //}
-                  if(object.hits > object.hitsMax / 3) {
-                    return false;
-                  }
-                  return true;
-                } 
-              });
-
-        if(target){
-          if(creep.repair(target) == ERR_NOT_IN_RANGE){
-            creep.moveTo(target);
-            return;
-          }
+        if(creep.room.controller) {
+          creep.moveTo(creep.room.controller);
+          creep.upgradeController(creep.room.controller);
         }
+
       }
 
     }
